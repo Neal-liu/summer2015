30d8a8c31c2a3ca4919929607cc67afa32be5aa3
Neal <nealliu.tw@gmail.com>
2015-08-15 22:57:20 +0800

    change data structures

--- phonebook/phonebook.h
+++ phonebook/phonebook.h
@@ -5,6 +5,11 @@
 
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+	struct __PHONE_BOOK_DETAIL *pDetail;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} PhoneBook;
+
+typedef struct __PHONE_BOOK_DETAIL {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -14,8 +19,7 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} PhoneBook;
+} PersonDetail;
 
 PhoneBook *findName(char last[], PhoneBook *pHead);
 PhoneBook *append(char *lastName, PhoneBook *entry);
